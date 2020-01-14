5892c7a0d930cd41d9a8d623eea32b1e661db8fb
Carlos Souza <carloshrsouza@gmail.com>
2020-01-14 17:36:07 -0500

    Add bulma and display data in a table

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import "./App.css";
+import "./App.sass";
 
 const URL = "https://frozen-peak-68797.herokuapp.com/winners"; // "http://localhost:8080/winners"
 
@@ -13,20 +13,43 @@ function App() {
   });
 
   return (
-    <div>
-      <p>FIFA Winners</p>
+    <div className="container">
+      <div className="hero is-light">
+        <div className="hero-body">
+          <div className="container">
+            <h1 className="title">FIFA World Cup Winners</h1>
+            <h3 className="subtitle">
+              List of countries who are bad ass in soccer
+            </h3>
+          </div>
+        </div>
+      </div>
       <WinnerList winners={winners}></WinnerList>
     </div>
   );
 }
 
 function WinnerList({ winners }) {
-  const list = winners.map((w, i) => <li key={i}>{w.country}</li>);
-  return (
-    <div className="winner-list">
-      <ul>{list}</ul>
-    </div>
+  const list = winners.map((w, i) => {
+    return (
+      <tr>
+        <td>{w.country}</td>
+        <td>{w.year}</td>
+      </tr>
+    );
+  });
+  const table = (
+    <table className="table is-bordered is-striped">
+      <thead>
+        <tr>
+          <th>Team</th>
+          <th>Year</th>
+        </tr>
+      </thead>
+      <tbody>{list}</tbody>
+    </table>
   );
+  return <div className="winner-list">{table}</div>;
 }
 
 export default App;
